30fbad046427418dd352dc9c9523896ab7e574f3
dimitra03 <t8210147@aueb.gr>
2024-08-08 15:31:34 +0300

    Create function.js with methods to move through pages when button is clicked

---
+++ src/main/webapp/static/js/functions.js
@@ -0,0 +1,7 @@
+function goToMainPage() {
+    window.location.href = '/TeachersWebApp/index.html'; // Replace with your desired URL
+}
+
+function goToSearchPage() {
+    window.location.href = '/TeachersWebApp/jsps/search.jsp'; // Replace with your desired URL
+}
