f9907f07203b084d45321262a8a2eb088a5cd9a1
dimitra03 <t8210147@aueb.gr>
2024-08-08 16:09:29 +0300

    Create insert.css

--- src/main/webapp/static/js/functions.js
+++ src/main/webapp/static/js/functions.js
@@ -5,3 +5,8 @@ function goToMainPage() {
 function goToSearchPage() {
     window.location.href = '/TeachersWebApp/jsps/search.jsp'; // Replace with your desired URL
 }
+
+
+function goToInsertPage() {
+    window.location.href = '/TeachersWebApp/jsps/insert.jsp'; // Replace with your desired URL
+}
