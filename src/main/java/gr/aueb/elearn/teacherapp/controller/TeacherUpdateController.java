e87dc16477adc012f8461059d14984207940f77a
dimitra03 <t8210147@aueb.gr>
2024-08-09 21:28:53 +0300

    Fix the Update controller

--- src/main/java/gr/aueb/elearn/teacherapp/controller/TeacherUpdateController.java
+++ src/main/java/gr/aueb/elearn/teacherapp/controller/TeacherUpdateController.java
@@ -1,11 +1,20 @@
 package gr.aueb.elearn.teacherapp.controller;
 
 import java.io.IOException;
+import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import gr.aueb.elearn.teacherapp.dao.ITeacherDAO;
+import gr.aueb.elearn.teacherapp.dao.TeacherDAOImpl;
+import gr.aueb.elearn.teacherapp.dto.TeacherDTO;
+import gr.aueb.elearn.teacherapp.service.ITeacherService;
+import gr.aueb.elearn.teacherapp.service.TeacherServiceImpl;
+import gr.aueb.elearn.teacherapp.service.exceptions.TeacherIdAlreadyExistsException;
+import gr.aueb.elearn.teacherapp.service.exceptions.TeacherNotFoundException;
 
 /**
  * Servlet implementation class TeacherUpdateController
@@ -16,7 +25,33 @@ public class TeacherUpdateController extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		TeacherDTO oldTeacherDTO;
+		TeacherDTO newTeacherDTO;
+		ITeacherDAO teacherDAO = new TeacherDAOImpl();
+		ITeacherService teacherServ = new TeacherServiceImpl(teacherDAO);
+		
+		response.setContentType("text/html");
+		int id = Integer.parseInt(request.getParameter("id"));
+		String oldLastName = request.getParameter("lastName");
+	    String oldFirstName = request.getParameter("firstName");
+	    String newLastName = request.getParameter("newLastName");
+	    String newFirstName = request.getParameter("newFirstName");
+		
+		oldTeacherDTO = new TeacherDTO(id, oldFirstName, oldLastName);
+		newTeacherDTO = new TeacherDTO(id, newFirstName, newLastName);
+		
+		try {
+			teacherServ.updateTeacher(oldTeacherDTO, newTeacherDTO);
+			request.setAttribute("updatedTeacher", newTeacherDTO);
+			request.getRequestDispatcher("/jsps/teacherupdated.jsp").forward(request, response);  //fix
+		} catch (SQLException e1) {
+			e1.printStackTrace();
+		} catch (TeacherNotFoundException e2) {
+			response.getWriter().write("<h1 style=\"color:red\">Teacher does not exist</h1>");
+			request.getRequestDispatcher("/jsps/teachers.jsp").include(request, response);
+		}	
+		
 		
 	}
 
