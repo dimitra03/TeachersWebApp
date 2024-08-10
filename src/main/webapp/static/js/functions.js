71da4d497597ccadcdd88f487e9ffe84d6e0f295
dimitra03 <t8210147@aueb.gr>
2024-08-10 19:05:51 +0300

    Add goToDeletePage() and goToUpdatePage()
    Also passes a message to the other page

--- src/main/webapp/static/js/functions.js
+++ src/main/webapp/static/js/functions.js
@@ -10,3 +10,13 @@ function goToSearchPage() {
 function goToInsertPage() {
     window.location.href = '/TeachersWebApp/jsps/insert.jsp'; // Replace with your desired URL
 }
+
+function goToDeletePage() {
+    var message = 'Search for the teacher you want to delete';
+    window.location.href = '/TeachersWebApp/jsps/search.jsp?mes=' + encodeURIComponent(message);  
+}
+
+function goToUpdatePage() {
+    var message = 'Search for the teacher you want to update';
+    window.location.href = '/TeachersWebApp/jsps/search.jsp?mes=' + encodeURIComponent(message);  
+}
