20dc37389296989e17e88e07590879ac3efd6bac
dimitra03 <t8210147@aueb.gr>
2024-08-15 15:58:20 +0300

    Update openConnection()
    take the db info from the application.properties

--- src/main/java/gr/aueb/elearn/teacherapp/dao/dbutil/DBUtil.java
+++ src/main/java/gr/aueb/elearn/teacherapp/dao/dbutil/DBUtil.java
@@ -1,10 +1,13 @@
 package gr.aueb.elearn.teacherapp.dao.dbutil;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class DBUtil {
 	
@@ -16,9 +19,24 @@ public class DBUtil {
 	private DBUtil() {}
 	
 	public static Connection openConnection() throws SQLException  {
-		String url = "jdbc:mysql://localhost:3306/teachersDB?serverTimezone=UTC&useSSL=false";
-		String username = "dimitratel";
-		String password = "2003";
+		// Load properties from application.properties
+        Properties props = new Properties();
+        try (InputStream input = DBUtil.class.getClassLoader().getResourceAsStream("application.properties")) {
+            if (input == null) {
+                throw new IOException("Sorry, unable to find application.properties");
+            }
+            // Load the properties file
+            props.load(input);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            throw new SQLException("Failed to load database configuration from application.properties", ex);
+        }
+
+        // Get database connection details from properties file
+        String url = props.getProperty("db.url");
+        String username = props.getProperty("db.username");
+        String password = props.getProperty("db.password");
+
 		
 		DriverManager.registerDriver(new com.mysql.jdbc.Driver());
 		conn =  DriverManager.getConnection(url, username, password);
